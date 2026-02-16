e8c5c7b3cafd8a971e48a1926e6ed33c0986115b
smilemathan <smile.mathan@gmail.com>
2026-02-16 14:54:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A;
+            double By = 22 / 7;
+            int r = 6;
+            A = By * r * r;
+            Console.WriteLine("Area of Circle is: " + A);
+            Console.ReadKey();
+        }
+    }
+}
